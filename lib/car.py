dd9f2b87cd3b0cccbb0d9a092abf84206a4b882c
Abigail Chelanagt <abigailchelah@gmail.com>
2025-05-11 21:50:24 +0300

    Add my files

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,5 @@
-from vehicle import Vehicle
+from vehicle import Vehicle  
 
-class Car(Vehicle):
-    pass
+class Car(Vehicle):  
+    def go(self):  
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
